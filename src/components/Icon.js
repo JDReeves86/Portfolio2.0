136b937e1040edc277bb7e4717af19a4c2d2c643
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-11-18 15:33:12 -0800

    fixed icon issue on mobile

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Icon({ attr }) {
     return (
-        <i className={`${attr} is-size-1 fa-2xl level-item pt-2 px-1 has-text-light`}></i>
+        <i className={`${attr} is-size-3 is-size-1-tablet fa-2xl level-item pt-2 my-3 px-1 has-text-light myIcon`}></i>
     )
 }
 
