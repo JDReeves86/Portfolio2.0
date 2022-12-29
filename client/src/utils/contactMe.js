cadd7480d8fd43b4a1722c1a291d3e89f95e5465
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-12-29 13:30:07 -0800

    netlify hates me

--- client/src/utils/contactMe.js
+++ client/src/utils/contactMe.js
@@ -1,9 +1,6 @@
 export const contactMe = async (input) => {
   const response = await fetch("/netlify/functions/nodemail.js", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
     body: JSON.stringify(input),
   });
   if (response.ok) {
