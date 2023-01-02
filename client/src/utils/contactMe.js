8665a6c0a9268dfe5b9800b85c2aa14dd212bf3f
JDReeves86 <Reeves.Jacobd@gmail.com>
2023-01-02 07:47:26 -0800

    moved function folder

--- client/src/utils/contactMe.js
+++ client/src/utils/contactMe.js
@@ -1,4 +1,5 @@
 export const contactMe = async (input) => {
+  console.log(input)
   const response = await fetch("/netlify/functions/nodemail.js", {
     method: "POST",
     body: JSON.stringify(input),
